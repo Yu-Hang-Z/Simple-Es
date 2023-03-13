f00320f5b14289b81a8d58d5161ad01473212e61
ZYHZ <Yu-Hang.Zhang@outlook.com>
2023-03-13 16:57:38 +0800

    feat(添加测试类):
    1.0.2版本：
    1.测试ES基本查询

---
+++ simple-es-test/src/test/java/com/zyhz/simpleestest/AggregationTest.java
@@ -0,0 +1,14 @@
+package com.zyhz.simpleestest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author by zhangyuhang
+ * @Classname AggregationTest
+ * @Description TODO
+ * @Date 2023/3/13 16:22
+ */
+@SpringBootTest
+public class AggregationTest {
+}
