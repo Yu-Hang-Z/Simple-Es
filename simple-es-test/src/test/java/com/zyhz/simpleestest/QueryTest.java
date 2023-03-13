12ea64305f412072e76d78bc5b5d33e348b955e3
ZYHZ <Yu-Hang.Zhang@outlook.com>
2023-03-13 16:21:56 +0800

    feat(添加测试类):
    1.0.2版本：
    1.测试ES基本查询

---
+++ simple-es-test/src/test/java/com/zyhz/simpleestest/QueryTest.java
@@ -0,0 +1,11 @@
+package com.zyhz.simpleestest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author by zhangyuhang
+ * @description: 测试类，测试ES基本查询
+ */
+@SpringBootTest
+public class QueryTest {
+}
